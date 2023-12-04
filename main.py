0cdf3b87412e880e37064f9c61983a1dfa80839e
ben <ben.lee8602@gmail.com>
2023-12-03 22:56:04 -0500

    implement preprocessing

--- main.py
+++ main.py
@@ -8,28 +8,26 @@ from src import preprocess
 def solve_maze(image):
     # preprocess
     bin = preprocess.binary_image(image)
-    corners = preprocess.detect_maze(bin)
-    if not corners:
-        return None
+    corners = preprocess.detect_maze(image)
     maze = preprocess.perspective_transform_maze(bin, corners)
-
+    
     # solve
     pass
 
     # display
     pass
 
-    return maze # temp
+    return image # temp
 
 
 # solve maze in the given file(s)
 for filename in sys.argv[1:]:
     image = cv2.imread(filename)
-    if not image:
+    if image is None:
         print(f"unable to load {filename}")
         continue
     sol = solve_maze(image)
-    cv2.imshow(f"maze solver ({filename})", sol if sol else image)
+    cv2.imshow(f"maze solver ({filename})", sol if sol is not None else image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -42,7 +40,7 @@ vid = cv2.VideoCapture(0)
 while True:
     _, frame = vid.read()
     sol = solve_maze(frame)
-    cv2.imshow("maze solver", sol if sol else frame)
+    cv2.imshow("maze solver", sol if sol is not None else frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,10 +1,52 @@
-MAZE_DSIZE = (600, 600)
+import cv2
+import numpy as np
 
-def binary_image(image):
-    pass
+
+def binary_image(image, thres=200):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+    image = cv2.adaptiveThreshold(
+        image, 255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY_INV,
+        11, 2
+    )
+    return image
+
 
 def detect_maze(image):
-    pass
+    edges = cv2.Canny(image, 50, 150)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-def perspective_transform_maze(image, corners, dsize=MAZE_DSIZE):
-    pass
+    largest_area = image.shape[0] * image.shape[1] // 8
+    largest_quad = None
+
+    for cnt in contours:
+        # approximate polygon
+        epsilon = 0.02 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
+
+        # if quadrilateral
+        if len(approx) == 4:
+            area = cv2.contourArea(approx)
+            if area > largest_area:
+                largest_area = area
+                largest_quad = approx
+    
+    if largest_quad is None:
+        return None
+    return np.float32([c[0] for c in largest_quad])
+
+
+def perspective_transform_maze(image, corners, dsize=400):
+    if corners is None:
+        return image
+    dest = np.float32([
+        [0, 0],
+        [dsize - 1, 0],
+        [dsize - 1, dsize - 1],
+        [0, dsize - 1]
+    ])
+    matrix = cv2.getPerspectiveTransform(corners, dest)
+    result = cv2.warpPerspective(image, matrix, (dsize, dsize))
+    return result
